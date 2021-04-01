068a5ef759f223c13bbdce7bd17f0ff35e7dc4b1
Inhoo Ham <inhooham90@gmail.com>
2021-03-31 23:26:24 -0400

    apply flatlist

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, SafeAreaView, StatusBar } from "react-native";
+import { View, SafeAreaView, StatusBar, FlatList } from "react-native";
 import styled from "styled-components";
 import { SearchBar } from "../../../components/SearchBar";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
@@ -18,9 +18,22 @@ export const RestaurantScreen = () => {
   return (
     <RestaurantScreenSafeAreaView>
       <SearchBar />
-      <RestaurantList>
+      <FlatList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+        ]}
+        renderItem={() => <RestaurantInfoCard />}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
+      {/* <RestaurantList>
         <RestaurantInfoCard />
-      </RestaurantList>
+      </RestaurantList> */}
     </RestaurantScreenSafeAreaView>
   );
 };
